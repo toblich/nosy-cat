151324817fd89d68c677307ed98a5d219144b50a
jmoguilevsky <jmoguilevsky@gmail.com>
2019-07-14 20:54:52 -0300

    ingress kafka

---
+++ ingress/src/kafka-integration.ts
@@ -0,0 +1,18 @@
+import { ProduceRequest } from "kafka-node";
+import { sendAsync, createTopicsAsync } from "@helpers/promisifed-kafka";
+
+const ping = async () => {
+  const payloads: ProduceRequest[] = [{ topic: "test", messages: ["test message"] }];
+
+  try {
+    const data = await sendAsync(payloads);
+
+    // tslint:disable-next-line:no-console
+    console.log("data", data);
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.error("error", error);
+  }
+};
+
+export { ping };
