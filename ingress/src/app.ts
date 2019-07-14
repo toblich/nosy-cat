51cc83a9f772f913539b8c6bcdb77de099c12e95
jmoguilevsky <jmoguilevsky@gmail.com>
2019-07-14 20:58:06 -0300

    integrate to /ping endpoint

--- ingress/src/app.ts
+++ ingress/src/app.ts
@@ -1,8 +1,12 @@
 import express = require("express");
+import { ping } from "./kafka-integration";
 
 const app: express.Application = express();
 
-app.get("/ping", (req, res) => res.send("pong"));
+app.get("/ping", (req, res) => {
+  ping();
+  res.send("pong");
+});
 
 // tslint:disable-next-line: no-console
 app.listen(3000, () => console.log("Listening on port 3000"));
