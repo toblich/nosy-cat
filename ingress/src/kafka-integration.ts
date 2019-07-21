29365198a6f37545cd9b801e1120acf77999368d
jmoguilevsky <jmoguilevsky@gmail.com>
2019-07-21 17:17:09 -0300

    Integrate ingress with kafka and externalize kafka (#9)
    * clean ingress
    
    * fix package lock
    
    * fix build
    
    * fix build
    
    * Update nodemon.json
    
    * Update tsconfig.json

--- ingress/src/kafka-integration.ts
+++ ingress/src/kafka-integration.ts
@@ -1,5 +1,5 @@
+import { sendAsync } from "@helpers/kafka";
 import { ProduceRequest } from "kafka-node";
-import { sendAsync } from "@helpers/promisifed-kafka";
 
 const ping = async () => {
   const payloads: ProduceRequest[] = [{ topic: "test", messages: ["test message"] }];
