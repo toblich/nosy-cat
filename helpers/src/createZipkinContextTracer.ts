381d7789a3044dfd7850f1812065a3a074673bbf
jmoguilevsky <jmoguilevsky@gmail.com>
2019-08-04 20:59:06 -0300

    add bodyparser

--- helpers/src/createZipkinContextTracer.ts
+++ helpers/src/createZipkinContextTracer.ts
@@ -2,7 +2,7 @@ import { Tracer, ExplicitContext, ConsoleRecorder } from "zipkin";
 
 export function createZipkinContextTracer(localServiceName: string) {
   const ctxImpl = new ExplicitContext();
-  const recorder = new ConsoleRecorder();
+  const recorder = new ConsoleRecorder(() => undefined);
   const tracer = new Tracer({ ctxImpl, recorder, localServiceName });
 
   return { ctx: ctxImpl, tracer };
--- helpers/src/createZipkinExpress.ts
+++ helpers/src/createZipkinExpress.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 import { Tracer } from "zipkin";
 import { expressMiddleware } from "zipkin-instrumentation-express";
 
 export function createZipkinExpress(tracer: Tracer) {
   const app = express();
+  app.use(bodyParser.json());
 
   // Add the Zipkin middleware
   app.use(expressMiddleware({ tracer }));
