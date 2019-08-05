faa294b3ce5c5247a980979340dcb73fff6958e4
jmoguilevsky <jmoguilevsky@gmail.com>
2019-08-04 21:03:48 -0300

    remove body parser

--- helpers/src/createZipkinExpress.ts
+++ helpers/src/createZipkinExpress.ts
@@ -1,12 +1,11 @@
 import * as express from "express";
-import * as bodyParser from "body-parser";
 
 import { Tracer } from "zipkin";
 import { expressMiddleware } from "zipkin-instrumentation-express";
 
 export function createZipkinExpress(tracer: Tracer) {
   const app = express();
-  app.use(bodyParser.json());
+  app.use(express.json());
 
   // Add the Zipkin middleware
   app.use(expressMiddleware({ tracer }));
