7bbbda6405c7ce7e68bea817d9feed2e649b7e53
jmoguilevsky <jmoguilevsky@gmail.com>
2019-09-01 20:12:28 -0300

    feature: add message processing and post to graph service

--- dependency-detector/src/app.ts
+++ dependency-detector/src/app.ts
@@ -1,12 +1,37 @@
 import { consume } from "./kafka-integration";
-import { logger, createZipkinContextTracer } from "helpers";
+import { logger, createZipkinContextTracer, ZipkinSpan, ComponentCall, Message, GraphClient } from "helpers";
 
 const { tracer } = createZipkinContextTracer("dependency-detector");
 
 consume(tracer, "ingress", onEveryMessage);
 
+const graphClient = new GraphClient("http://localhost:4000");
+
 // ---
 
-async function onEveryMessage({ partition, message }) {
+const processSpan = (span: ZipkinSpan): ComponentCall => ({
+  callee: span.localEndpoint.serviceName,
+  caller: (span.remoteEndpoint && span.remoteEndpoint.serviceName) || undefined
+});
+
+function checkDependency(span: ZipkinSpan): ComponentCall[] {
+  return [processSpan(span)];
+}
+
+function checkBulkDependencies(spans: ZipkinSpan[]): ComponentCall[] {
+  return spans.map(processSpan);
+}
+
+async function onEveryMessage({ partition, message }: { partition: any; message: Message }) {
   logger.info(JSON.stringify({ partition, offset: message.offset, value: message.value.toString() }));
+
+  let componentCalls: ComponentCall[];
+
+  if (Array.isArray(message.value)) {
+    componentCalls = checkBulkDependencies(message.value);
+  } else {
+    componentCalls = checkDependency(message.value);
+  }
+
+  await graphClient.postComponentCalls(componentCalls);
 }
---
+++ helpers/src/graphClient.ts
@@ -0,0 +1,20 @@
+import { ComponentCall, GraphServiceRequestBody } from "./types";
+
+class GraphClient {
+  private url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  public postComponentCalls = (componentCalls: ComponentCall[]) => {
+    const requestBody: GraphServiceRequestBody = { componentCalls };
+
+    return fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(requestBody)
+    });
+  };
+}
+
+export { GraphClient };
