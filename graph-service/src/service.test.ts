50bbad28e748648f9ae3435277a47813ece8548a
toblich <toblich@gmail.com>
2019-09-14 16:45:20 -0300

    Add component search

--- graph-service/src/service.test.ts
+++ graph-service/src/service.test.ts
@@ -1,24 +1,62 @@
+import * as httpErrors from "http-errors";
 import * as service from "./service";
+import { Status } from "./Graph";
 
-test("service", () => {
-  const toAdd = [
-    { caller: "A", callee: "B" },
-    { caller: "C" },
-    { callee: "D" },
-    { caller: "A", callee: "E" },
-    { caller: "E", callee: "B" }
-  ];
+describe("service", () => {
+  beforeEach(service.clear);
 
-  const expected = {
-    A: { dependencies: ["B", "E"], status: "OK" },
-    B: { dependencies: [], status: "OK" },
-    C: { dependencies: [], status: "OK" },
-    D: { dependencies: [], status: "OK" },
-    E: { dependencies: ["B"], status: "OK" }
-  };
+  describe("a complete case", () => {
+    let toAdd;
+    let expected;
 
-  service.clear();
+    beforeEach(() => {
+      toAdd = [
+        { caller: "A", callee: "B" },
+        { caller: "C" },
+        { callee: "D" },
+        { caller: "A", callee: "E" },
+        { caller: "E", callee: "B" }
+      ];
 
-  toAdd.forEach(service.add);
-  expect(service.toPlainObject()).toEqual(expected);
+      expected = {
+        A: { dependencies: ["B", "E"], status: "OK" },
+        B: { dependencies: [], status: "OK" },
+        C: { dependencies: [], status: "OK" },
+        D: { dependencies: [], status: "OK" },
+        E: { dependencies: ["B"], status: "OK" }
+      };
+
+      toAdd.forEach(service.add);
+    });
+
+    it("should deep equal the expected graph", () => {
+      expect(service.toPlainObject()).toEqual(expected);
+    });
+  });
+
+  describe("getPlain", () => {
+    const existingId = "ExistingComponent";
+    const missingId = "MissingComponent";
+
+    beforeEach(() => {
+      service.add({ caller: existingId });
+    });
+
+    describe("when getting a component that does exist", () => {
+      it("should return the component", () => {
+        const component = service.getPlain(existingId);
+        expect(component).toEqual({
+          id: existingId,
+          dependencies: [],
+          status: Status.OK
+        });
+      });
+    });
+
+    describe("when trying to get a component that does not exist", () => {
+      it("should throw a NotFound error", () => {
+        expect(() => service.getPlain(missingId)).toThrow(httpErrors.NotFound);
+      });
+    });
+  });
 });
