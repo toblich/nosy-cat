6e8c4b89b285937a750538db494fc1e4e1918582
Tobías Lichtig <toblich@users.noreply.github.com>
2019-09-14 18:02:59 -0300

    [Graph Service] Extract controller and middlewares (#36)

--- graph-service/src/app.ts
+++ graph-service/src/app.ts
@@ -1,7 +1,8 @@
 import * as express from "express";
 import { logger, createZipkinContextTracer, createZipkinExpress } from "helpers";
 
-import * as service from "./service";
+import * as controller from "./controller";
+import * as middlewares from "./middlewares";
 
 const { tracer } = createZipkinContextTracer("graph-service");
 
@@ -9,77 +10,12 @@ const { tracer } = createZipkinContextTracer("graph-service");
 
 const app: express.Application = createZipkinExpress(tracer);
 
-// tslint:disable-next-line: typedef
-app.use("*", (req, res, next) => {
-  logger.debug(`${coerceReqTrace(req)} => ${coerceReqDetails(req)}`);
-  (req as any)._startTime = Date.now();
+app.use(middlewares.logging);
 
-  const originalSend = res.send;
-  (res as any).send = (body: any): void => {
-    if (res.statusCode < 400) {
-      const duration = Date.now() - (res.req as any)._startTime;
-      logger.debug(`${coerceReqTrace(res.req)} <= ${res.statusCode} after ${duration}ms with ${toJSON(body)}`);
-    }
-    return originalSend.call(res, body);
-  };
+app.post("/graph", controller.addComponentsAndDependencies);
+app.get("/graph", controller.getGraphAsJson);
+app.post("/graph/search", controller.searchComponent);
 
-  next();
-});
-
-// tslint:disable-next-line: typedef
-app.post("/graph", (req, res, next) => {
-  try {
-    req.body.map(service.add);
-  } catch (e) {
-    return next(e);
-  }
-
-  res.status(201).send();
-});
-
-// tslint:disable-next-line: typedef
-app.get("/graph", (_, res) => {
-  res.json(service.toPlainObject());
-});
-
-// tslint:disable-next-line: typedef
-app.post("/graph/search", (req, res, next) => {
-  let component;
-  try {
-    component = service.getPlain(req.body.component);
-  } catch (e) {
-    return next(e);
-  }
-
-  res.json(component);
-});
-
-// tslint:disable-next-line: typedef
-app.use(function globalErrorHandlingMiddleware(err, req, res, _) {
-  const status = err.status || 500;
-
-  logger.error(`${coerceReqTrace(req)} ${err.stack}`);
-
-  res.status(status).json({
-    status,
-    name: err.name,
-    message: err.message,
-    properties: err.properties || err.stack || {}
-  });
-});
+app.use(middlewares.globalErrorHandling);
 
 app.listen(4000, () => logger.info("Graph listening on port 4000"));
-
-// ---
-
-function coerceReqTrace(req: any): string {
-  return `[traceId=${req._trace_id.traceId}] [spanId=${req._trace_id.spanId}]`;
-}
-
-function coerceReqDetails(req: express.Request): string {
-  return `${req.method} ${req.originalUrl} ${toJSON(req.body)}`;
-}
-
-function toJSON(body: any): string {
-  return body ? JSON.stringify(body).replace(/\\"/g, "") : "no body";
-}
