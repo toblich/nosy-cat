9a3dff7b10969aefe30306688d3305eecf96b5d9
jmoguilevsky <jmoguilevsky@gmail.com>
2019-09-22 20:04:21 -0300

    Update ingress/src/kafka-integration.ts
    Co-Authored-By: Tobías Lichtig <toblich@users.noreply.github.com>

--- ingress/src/kafka-integration.ts
+++ ingress/src/kafka-integration.ts
@@ -6,7 +6,7 @@ const spans = async (producer: Producer, body: any) => {
   try {
     logger.info(`new message ${JSON.stringify(body)}`);
 
-    await producer.send({ acks: -1, topic: "ingress", messages: [{ value: JSON.stringify(body) }] });
+    await producer.send({ topic: "ingress", messages: [{ value: JSON.stringify(body) }] });
     logger.info("sent message");
   } catch (error) {
     logger.error("couldn't send message", error);
