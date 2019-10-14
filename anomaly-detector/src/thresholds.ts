e962b7f359d4c6a8dfb69d96305aecf1dbb04b69
jmoguilevsky <jmoguilevsky@gmail.com>
2019-10-14 18:55:48 -0300

    pr feedback

--- anomaly-detector/src/thresholds.ts
+++ anomaly-detector/src/thresholds.ts
@@ -3,12 +3,12 @@ import { ServiceThresholds } from "./types";
 const thresholds: ServiceThresholds = {
   test: {
     errorRate: 0.5,
-    responseTime: 0.5,
+    meanResponseTimeMs: 0.5,
     throughput: 0.5
   },
   test2: {
     errorRate: 0.5,
-    responseTime: 0.5,
+    meanResponseTimeMs: 0.5,
     throughput: 0.5
   }
 };
