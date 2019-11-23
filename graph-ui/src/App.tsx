b5020d4117b8ab61de69718ecefbd934e41ba624
jmoguilevsky <jmoguilevsky@gmail.com>
2019-11-23 16:31:11 -0300

    add simple ui

--- graph-ui/src/App.tsx
+++ graph-ui/src/App.tsx
@@ -1,21 +1,76 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Graph from "react-graph-vis";
+import { Options } from "vis";
+import { connect } from "socket.io-client";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const graph = {
+  nodes: [],
+  edges: []
 };
+
+const options: Options = {
+  layout: {
+    hierarchical: true
+  },
+  edges: {
+    color: "#000000"
+  },
+  height: "100%",
+  autoResize: false,
+  physics: {
+    enabled: false
+  },
+  clickToUse: false,
+  interaction: {
+    dragNodes: false
+  }
+};
+
+const events = {
+  select: (event: any): void => {
+    const { nodes, edges } = event;
+    console.log(nodes, edges);
+  }
+};
+
+// const prettify = (graph) => {}
+
+class App extends React.PureComponent<any, any> {
+  private socket: SocketIOClient.Socket;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      graph
+    };
+
+    this.socket = connect("http://localhost:51083");
+  }
+
+  public componentDidMount(): void {
+    this.socket.on("graph", (newGraph: any): void => {
+      // console.log("graph sent", newGraph);
+      this.setState({
+        graph: newGraph
+      });
+    });
+
+    setInterval(() => {
+      this.socket.send("joni");
+    }, 10000);
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Graph visualization</h1>
+        </header>
+        <Graph graph={this.state.graph} options={options} events={events} />
+      </div>
+    );
+  }
+}
 
 export default App;
