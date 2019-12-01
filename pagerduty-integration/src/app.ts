b210ed8117c9c6d312428d3a5432910789ba58cf
toblich <toblich@gmail.com>
2019-12-01 19:22:20 -0300

    Improve PD logs

--- pagerduty-integration/src/app.ts
+++ pagerduty-integration/src/app.ts
@@ -14,9 +14,10 @@ process.on("SIGINT", () => {
   while (shouldContinue) {
     const message = await consumer.receive();
     consumer.acknowledge(message);
-    logger.info(message.getData());
+    logger.data(`Got Pulsar message: ${message.getData()}`);
 
     try {
+      // https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
       const res = await superagent.post("https://events.pagerduty.com/v2/enqueue").send({
         routing_key: "6e05660959644cedabd1afb3c8abf8b8",
         event_action: "trigger",
@@ -29,62 +30,11 @@ process.on("SIGINT", () => {
           class: "test class"
         }
       });
-      logger.info(`${res.status} ${JSON.stringify(res.body, null, 4)}`);
+      logger.info(`PagerDuty responded with: ${res.status} ${JSON.stringify(res.body, null, 4)}`);
     } catch (e) {
-      logger.error(e.message + JSON.stringify(e.body, null, 4));
+      logger.error(`PagerDuty Error: ${e.message} ${JSON.stringify(e.body, null, 4)}`);
     }
   }
-
-  /*
-routing_key:
-required
-string
-This is the 32 character Integration Key for an integration on a service or on a global ruleset.
-
-event_action:
-required
-stringtrigger
-The type of event. Can be trigger, acknowledge or resolve. See Event Action.
-
-dedup_key:	string
-Deduplication key for correlating triggers and resolves. The maximum permitted length of this property is 255 characters.
-
-payload.summary:
-required
-string
-A brief text summary of the event, used to generate the summaries/titles of any associated alerts. The maximum permitted length of this property is 1024 characters.
-
-payload.source:
-required
-string
-The unique location of the affected system, preferably a hostname or FQDN.
-
-payload.severity:
-required
-string
-The perceived severity of the status the event is describing with respect to the affected system. This can be critical, error, warning or info.
-
-payload.timestamp:	timestamp
-The time at which the emitting tool detected or generated the event.
-
-payload.component:	string
-Component of the source machine that is responsible for the event, for example mysql or eth0
-
-payload.group:	string
-Logical grouping of components of a service, for example app-stack
-
-payload.class:	string
-The class/type of the event, for example ping failure or cpu load
-
-payload.custom_details:	object
-Additional details about the event and affected system
-
-images:	array of objects
-List of images to include.
-
-links:	array of objects
-List of links to include.
-  */
 
   await consumer.shutdown();
 })();
