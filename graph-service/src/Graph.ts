cd85354f8a5e02f7eb664134fe89331091264c9a
toblich <toblich@gmail.com>
2020-03-11 11:55:58 -0300

    Add dep and write model explanation

--- graph-service/src/Graph.ts
+++ graph-service/src/Graph.ts
@@ -1,5 +1,13 @@
 import { ComponentStatus, ComponentPlainObject } from "helpers";
 
+// Make all nodes have the label Component
+// Then, make them all have another label (or property?) with the status
+// All of them should have a property "id: string"
+// Then, have an index on Component(id)
+// and a constraint on uniqueness of Component(id)
+
+// All relations should have the same relation type - CALLS
+
 export class Component {
   public id: string;
   public dependencies: Set<string>;
