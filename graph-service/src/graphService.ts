5e74c090fcaaf4c2e95cc60f229451182334dcdf
toblich <toblich@gmail.com>
2020-05-11 00:57:37 -0300

    Minor cleanup

--- graph-service/src/graphService.ts
+++ graph-service/src/graphService.ts
@@ -17,7 +17,6 @@ import {
 
 import Repository, { Result, Record, Transaction } from "./repository";
 import { Component } from "./Graph";
-// import { Node } from "neo4j-driver";
 
 export const defaultTestMetrics = {
   duration: 1,
@@ -121,29 +120,29 @@ repository.clear().then(async () => {
   // }
 });
 
-async function testHelper(initialId: string, operation: string, expectedUnsorted: string): Promise<void> {
-  if (!["Root causes", "Causal chain"].includes(operation)) {
-    throw new Error(`Unsupported test operation: ${operation}`);
-  }
-  const expected = Array.from(expectedUnsorted).sort().join("");
+// async function testHelper(initialId: string, operation: string, expectedUnsorted: string): Promise<void> {
+//   if (!["Root causes", "Causal chain"].includes(operation)) {
+//     throw new Error(`Unsupported test operation: ${operation}`);
+//   }
+//   const expected = Array.from(expectedUnsorted).sort().join("");
 
-  const idsArray =
-    operation === "Root causes"
-      ? (await findRootCauses(initialId)).map((r: Node) => r.id)
-      : (await findCausalChain(initialId)).map((r: Node) => r.id);
+//   const idsArray =
+//     operation === "Root causes"
+//       ? (await findRootCauses(initialId)).map((r: Node) => r.id)
+//       : (await findCausalChain(initialId)).map((r: Node) => r.id);
 
-  const results = idsArray.sort().join("");
+//   const results = idsArray.sort().join("");
 
-  if (results === expected) {
-    logger.info(`${operation} for ${initialId} are the expected ones!`);
-  } else {
-    logger.error(`${operation} for ${initialId} did not match
-      Expected: "${expected}"
-      Actual:   "${results}"
-    `);
-    throw new Error("FailedTest");
-  }
-}
+//   if (results === expected) {
+//     logger.info(`${operation} for ${initialId} are the expected ones!`);
+//   } else {
+//     logger.error(`${operation} for ${initialId} did not match
+//       Expected: "${expected}"
+//       Actual:   "${results}"
+//     `);
+//     throw new Error("FailedTest");
+//   }
+// }
 
 export async function clear(): Promise<void> {
   return repository.clear();
@@ -184,16 +183,18 @@ export async function findCausalChain(initialId: string, tx?: Transaction): Prom
 
 export async function findRootCauses(initialId: string): Promise<Node[]> {
   const tx = repository.transaction();
+
   let abnormalSubgraph;
   try {
     const chain = await findCausalChain(initialId, tx);
+
     if (chain.length === 0) {
       await tx.commit();
       return [];
     }
 
     abnormalSubgraph = await toEntity(initialId, chain, tx);
-    // logger.data(`Abnormal subgraph from ${initialId}: ${JSON.stringify(abnormalSubgraph, null, 4)}`);
+
     await tx.commit();
   } catch (e) {
     logger.error(e);
@@ -257,19 +258,14 @@ function internalDFS(
   parentId: string = null
 ): Node[] {
   let component: Node = subgraph[id];
-  // logger.error(`Starting DFS from id ${id}. Path: ${path.map((n: Node) => n.id)}. Parent: ${parentId}. Visited: ${Array.from(visited)}`);
 
   if (!component || (parentId && !subgraph[parentId])) {
     // Case for when accessing a node that has been merged into a supernode
-    // logger.error("CASE OF SUPERNODE!");
     return [];
   }
 
   if (visited.has(id)) {
     // * Cycle detected!
-    // logger.warn(`Cycle detected for id ${id}`);
-    // logger.warn(`Visited: ${Array.from(visited)}`);
-    // logger.warn(`Path: ${path.map((n: Node) => n.id)}`);
     // Since the path array has the ordered list of components visited to reach the current component,
     // take the components visited since the first time that we visited the current one. That's the cycle
     // that has formed and was detected.
@@ -293,25 +289,19 @@ function internalDFS(
     // Remove existing nodes of the cycle merged into supernode from the graph an the visited set
     // Also mark dependencies as not visited
     for (const cycleId of cycleIds) {
-      // logger.info(`removing ${cycleId} from visited`);
       visited.delete(cycleId);
       for (const depId of subgraph[cycleId].dependencies) {
-        // logger.info(`removing ${depId} from visited`);
         visited.delete(depId);
       }
       delete subgraph[cycleId];
     }
     // Remove all visited nodes that are path of the cycle, as they will be re-added as just the supernode
-    // logger.data(`path        : ${JSON.stringify(path.map((n: Node) => n.id))}`);
-    path.splice(path.length - cycle.length); // TODO check if there is an off-by-one bug here
-    // logger.data(`path spliced: ${JSON.stringify(path.map((n: Node) => n.id))}`);
+    path.splice(path.length - cycle.length);
 
     // For nodes that depended on something in the cycle, change that dep to be on the supernode
     for (const node of Object.values(subgraph)) {
-      // logger.data(`Evaluating deps of ${node.id}`);
       node.depsSet = new Set(node.dependencies.map((dep: string) => (cycleIdsSet.has(dep) ? supernodeId : dep)));
       node.dependencies = Array.from(node.depsSet);
-      // logger.data(`new depSet: ${JSON.stringify(node.dependencies)}`);
     }
 
     // Add supernode to graph
@@ -355,11 +345,4 @@ interface Supernode extends Node {
 
 export async function getFullGraph(): Promise<Result> {
   return repository.getFullGraph();
-}
-
-// TODO delete when no longer used
-class NotImplementedError extends Error {
-  constructor() {
-    super("Not Implemented :(");
-  }
 }
