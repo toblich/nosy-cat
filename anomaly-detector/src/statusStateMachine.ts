5d76c1057ace21f4edc87a1b2723d802123c1272
jmoguilevsky <jmoguilevsky@gmail.com>
2020-09-10 16:15:19 -0300

    fix state machine

--- anomaly-detector/src/statusStateMachine.ts
+++ anomaly-detector/src/statusStateMachine.ts
@@ -3,77 +3,13 @@ import { ComponentStatus } from "helpers";
 const MAX_CONFIRMED_COUNT = 3;
 const MAX_HEALED_COUNT = 3;
 
-function isStatusHealthy(componentStatus: ComponentStatus): boolean {
-  return [ComponentStatus.NORMAL, ComponentStatus.SUSPICIOUS].includes(componentStatus);
-}
-
-function isStatusConfirmedHealthy(componentStatus: ComponentStatus): boolean {
-  return [ComponentStatus.NORMAL].includes(componentStatus);
-}
-
-function isStatusConfirmedAnomalous(componentStatus: ComponentStatus): boolean {
-  return [ComponentStatus.CONFIRMED, ComponentStatus.PERPETRATOR, ComponentStatus.VICTIM].includes(componentStatus);
-}
-
-function isStatusAnomalous(componentStatus: ComponentStatus): boolean {
-  return [
-    ComponentStatus.CONFIRMED,
-    ComponentStatus.PERPETRATOR,
-    ComponentStatus.VICTIM,
-    ComponentStatus.HEALING
-  ].includes(componentStatus);
-}
-
-//  NORMAL = "NORMAL",
-// SUSPICIOUS = "SUSPICIOUS",
-// CONFIRMED = "CONFIRMED",
-// VICTIM = "VICTIM",
-// PERPETRATOR = "PERPETRATOR",
-// HEALING = "HEALING"
 export function getNewStatus(
   currentStatus: ComponentStatus,
   statusOccurrences: number,
   hasErrored: boolean
-): { newStatus: ComponentStatus } {
-  if (currentStatus === ComponentStatus.NORMAL && !hasErrored) {
-    return { newStatus: ComponentStatus.NORMAL };
-  }
-
-  if (isStatusConfirmedAnomalous(currentStatus) && hasErrored) {
-    return { newStatus: currentStatus };
-  }
-
-  if (isStatusHealthy(currentStatus) && hasErrored) {
-    if (currentStatus === ComponentStatus.NORMAL) {
-      return { newStatus: ComponentStatus.SUSPICIOUS };
-    }
-
-    if (statusOccurrences + 1 < MAX_CONFIRMED_COUNT) {
-      // TODO: increase status occurrences
-      return { newStatus: currentStatus };
-    }
-
-    // TODO: calculate confirmed status
-    return { newStatus: ComponentStatus.CONFIRMED };
-  }
-
-  if (isStatusConfirmedAnomalous(currentStatus) && !hasErrored) {
-    if (currentStatus !== ComponentStatus.HEALING) {
-      return { newStatus: ComponentStatus.HEALING };
-    }
-
-    if (statusOccurrences + 1 < CONFIRMED_COUNT) {
-      // TODO: increase status occurrences
-      return { newStatus: currentStatus };
-    }
-
-    // TODO: calculate confirmed status
-    return { newStatus: ComponentStatus.CONFIRMED };
-  }
-
-  return {
-    newStatus: ComponentStatus.CONFIRMED
-  };
+): StateResponse {
+  const method = hasErrored ? "error" : "normal";
+  return stateMap[currentStatus].events[method](statusOccurrences);
 }
 
 interface StateResponse {
@@ -84,11 +20,11 @@ interface StateResponse {
 const stateMap = {
   [ComponentStatus.NORMAL]: {
     events: {
-      error: (): StateResponse => ({
+      error: (_: number): StateResponse => ({
         status: ComponentStatus.SUSPICIOUS,
         occurrences: 0
       }),
-      normal: (): StateResponse => ({ status: ComponentStatus.NORMAL, occurrences: 0 })
+      normal: (_: number): StateResponse => ({ status: ComponentStatus.NORMAL, occurrences: 0 })
     }
   },
   [ComponentStatus.SUSPICIOUS]: {
@@ -111,7 +47,7 @@ const stateMap = {
   [ComponentStatus.PERPETRATOR]: anomalousState(ComponentStatus.PERPETRATOR),
   [ComponentStatus.HEALING]: {
     events: {
-      error: {},
+      error: (): StateResponse => ({ status: ComponentStatus.CONFIRMED, occurrences: 0 }),
       normal: (occurrences: number): StateResponse =>
         occurrences + 1 < MAX_HEALED_COUNT
           ? {
@@ -126,10 +62,12 @@ const stateMap = {
   }
 };
 
-function anomalousState(status: ComponentStatus): any {
+function anomalousState(
+  status: ComponentStatus
+): { events: { error: (_: number) => StateResponse; normal: () => StateResponse } } {
   return {
     events: {
-      error: (): StateResponse => ({ status, occurrences: 0 }),
+      error: (_: number): StateResponse => ({ status, occurrences: 0 }),
       normal: (): StateResponse => ({
         status: ComponentStatus.HEALING,
         occurrences: 0
