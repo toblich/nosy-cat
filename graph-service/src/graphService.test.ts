130c861bd16b7af69963087e43f6724b24fbe750
jmoguilevsky <jmoguilevsky@gmail.com>
2021-02-13 19:23:09 -0300

    add topologies tests

--- graph-service/src/graphService.test.ts
+++ graph-service/src/graphService.test.ts
@@ -228,7 +228,7 @@ describe("new tests", () => {
       });
 
       describe("applying changes to both", () => {
-        initialize(graph, true);
+        initialize(graph);
         test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
         test("B", CONFIRMED, change("B", NORMAL, PERPETRATOR));
         // both CONFIRMED now
@@ -304,11 +304,280 @@ describe("new tests", () => {
       // Test 3.14.b
       test("C", NORMAL, merge(change("B", PERPETRATOR, VICTIM), change("C", PERPETRATOR, NORMAL)));
     });
+
+    // Case 3.15
+    describe("Cycle is part of bigger cycle A <- B <-> C ; A -> C", () => {
+      initialize({ A: ["C"], B: ["A", "C"], C: ["B"] });
+
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+      test("B", CONFIRMED, change("B", NORMAL, VICTIM));
+
+      // Test 3.15.a
+      test("C", CONFIRMED, merge(change("B", VICTIM, PERPETRATOR), change("C", NORMAL, PERPETRATOR)));
+
+      // Test 3.15.b
+      test("C", NORMAL, merge(change("B", PERPETRATOR, VICTIM), change("C", PERPETRATOR, NORMAL)));
+
+      test("A", NORMAL, merge(change("A", PERPETRATOR, NORMAL), change("B", VICTIM, PERPETRATOR)));
+      test("C", CONFIRMED, change("C", NORMAL, PERPETRATOR));
+
+      // Test 3.15.c
+      test("A", CONFIRMED, merge(change("A", NORMAL, PERPETRATOR)));
+
+      // Test 3.15.d
+      test("A", NORMAL, merge(change("A", PERPETRATOR, NORMAL)));
+
+      test("B", NORMAL, change("B", PERPETRATOR, NORMAL));
+      test("A", CONFIRMED, change("A", NORMAL, VICTIM));
+
+      // Test 3.15.e
+      test("B", CONFIRMED, merge(change("A", VICTIM, PERPETRATOR), change("B", NORMAL, PERPETRATOR)));
+
+      // Test 3.15.f
+      test("B", NORMAL, merge(change("A", PERPETRATOR, VICTIM), change("B", PERPETRATOR, NORMAL)));
+    });
+
+    // Case 3.27
+    describe("3-complete graph", () => {
+      initialize({ A: ["B", "C"], B: ["A", "C"], C: ["A", "B"] });
+      test("B", CONFIRMED, change("B", NORMAL, PERPETRATOR));
+      test("C", CONFIRMED, change("C", NORMAL, PERPETRATOR));
+
+      // Test 3.27.a
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+      // Test 3.27.b
+      test("A", NORMAL, change("A", PERPETRATOR, NORMAL));
+
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+
+      // Test 3.27.c
+      test("B", NORMAL, change("B", PERPETRATOR, NORMAL));
+      // Test 3.27.d
+      test("B", CONFIRMED, change("B", NORMAL, PERPETRATOR));
+    });
+  });
+
+  describe("Multi-node graph", () => {
+    // Case 4.1
+    describe("D depends on ring", () => {
+      initialize({ A: ["B"], B: ["C"], C: ["D"], D: ["E"], E: ["A"], F: ["A"] });
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+      test("B", CONFIRMED, merge(change("A", PERPETRATOR, VICTIM), change("B", NORMAL, PERPETRATOR)));
+      test("C", CONFIRMED, merge(change("B", PERPETRATOR, VICTIM), change("C", NORMAL, PERPETRATOR)));
+      test("D", CONFIRMED, merge(change("C", PERPETRATOR, VICTIM), change("D", NORMAL, PERPETRATOR)));
+      test(
+        "E",
+        CONFIRMED,
+        merge(
+          change("A", VICTIM, PERPETRATOR),
+          change("B", VICTIM, PERPETRATOR),
+          change("C", VICTIM, PERPETRATOR),
+          change("E", NORMAL, PERPETRATOR)
+        )
+      );
+
+      // Case 4.1.a
+      test("F", CONFIRMED, merge(change("F", NORMAL, VICTIM)));
+
+      // Case 4.1.b
+      test("F", NORMAL, merge(change("F", VICTIM, NORMAL)));
+    });
+
+    // Case 4.2
+    describe("Ring depends on F", () => {
+      initialize({ A: ["B", "F"], B: ["C"], C: ["D"], D: ["E"], E: ["A"], F: [] });
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+      test("B", CONFIRMED, merge(change("A", PERPETRATOR, VICTIM), change("B", NORMAL, PERPETRATOR)));
+      test("C", CONFIRMED, merge(change("B", PERPETRATOR, VICTIM), change("C", NORMAL, PERPETRATOR)));
+      test("D", CONFIRMED, merge(change("C", PERPETRATOR, VICTIM), change("D", NORMAL, PERPETRATOR)));
+      test(
+        "E",
+        CONFIRMED,
+        merge(
+          change("A", VICTIM, PERPETRATOR),
+          change("B", VICTIM, PERPETRATOR),
+          change("C", VICTIM, PERPETRATOR),
+          change("E", NORMAL, PERPETRATOR)
+        )
+      );
+
+      // Case 4.2.a
+      test(
+        "F",
+        CONFIRMED,
+        merge(
+          change("A", PERPETRATOR, VICTIM),
+          change("B", PERPETRATOR, VICTIM),
+          change("C", PERPETRATOR, VICTIM),
+          change("D", PERPETRATOR, VICTIM),
+          change("E", PERPETRATOR, VICTIM),
+          change("F", NORMAL, PERPETRATOR)
+        )
+      );
+
+      // Case 4.2.b
+      test(
+        "F",
+        NORMAL,
+        merge(
+          change("A", VICTIM, PERPETRATOR),
+          change("B", VICTIM, PERPETRATOR),
+          change("C", VICTIM, PERPETRATOR),
+          change("D", VICTIM, PERPETRATOR),
+          change("E", VICTIM, PERPETRATOR),
+          change("F", PERPETRATOR, NORMAL)
+        )
+      );
+
+      test(
+        "F",
+        CONFIRMED,
+        merge(
+          change("A", PERPETRATOR, VICTIM),
+          change("B", PERPETRATOR, VICTIM),
+          change("C", PERPETRATOR, VICTIM),
+          change("D", PERPETRATOR, VICTIM),
+          change("E", PERPETRATOR, VICTIM),
+          change("F", NORMAL, PERPETRATOR)
+        )
+      );
+
+      // Case 4.2.c
+      test("A", NORMAL, merge(change("A", VICTIM, NORMAL), change("E", VICTIM, PERPETRATOR)));
+
+      // Case 4.2.d
+      test("A", CONFIRMED, merge(change("A", NORMAL, VICTIM), change("E", PERPETRATOR, VICTIM)));
+    });
+
+    // Case 4.3
+    describe("Two rings with a shared chain (loop A through F + loop EHIJBCDE [BCDE exist in both loops])", () => {
+      initialize({
+        A: ["B"],
+        B: ["C"],
+        C: ["D"],
+        D: ["E"],
+        E: ["F", "H"],
+        F: ["G"],
+        G: ["A"],
+        H: ["I"],
+        I: ["J"],
+        J: ["B"],
+      });
+
+      // Setting first loop as abnormal (A through F)
+      test("A", CONFIRMED, change("A", NORMAL, PERPETRATOR));
+      test("B", CONFIRMED, merge(change("A", PERPETRATOR, VICTIM), change("B", NORMAL, PERPETRATOR)));
+      test("C", CONFIRMED, merge(change("B", PERPETRATOR, VICTIM), change("C", NORMAL, PERPETRATOR)));
+      test("D", CONFIRMED, merge(change("C", PERPETRATOR, VICTIM), change("D", NORMAL, PERPETRATOR)));
+      test("E", CONFIRMED, merge(change("D", PERPETRATOR, VICTIM), change("E", NORMAL, PERPETRATOR)));
+      test("F", CONFIRMED, merge(change("E", PERPETRATOR, VICTIM), change("F", NORMAL, PERPETRATOR)));
+      test(
+        "G",
+        CONFIRMED,
+        merge(
+          change("A", VICTIM, PERPETRATOR),
+          change("B", VICTIM, PERPETRATOR),
+          change("C", VICTIM, PERPETRATOR),
+          change("D", VICTIM, PERPETRATOR),
+          change("E", VICTIM, PERPETRATOR),
+          change("G", NORMAL, PERPETRATOR)
+        )
+      );
+
+      // Setting the other loop as abnormal except H
+      test("I", CONFIRMED, change("I", NORMAL, PERPETRATOR));
+      test("J", CONFIRMED, merge(change("I", PERPETRATOR, VICTIM), change("J", NORMAL, VICTIM)));
+
+      // Test 4.3.a
+      test(
+        "H",
+        CONFIRMED,
+        merge(change("H", NORMAL, PERPETRATOR), change("I", VICTIM, PERPETRATOR), change("J", VICTIM, PERPETRATOR))
+      );
+
+      // Test 4.3.b
+      test(
+        "H",
+        NORMAL,
+        merge(change("H", PERPETRATOR, NORMAL), change("I", PERPETRATOR, VICTIM), change("J", PERPETRATOR, VICTIM))
+      );
+
+      test(
+        "H",
+        CONFIRMED,
+        merge(change("H", NORMAL, PERPETRATOR), change("I", VICTIM, PERPETRATOR), change("J", VICTIM, PERPETRATOR))
+      );
+
+      // Test 4.3.c
+      test(
+        "B",
+        NORMAL,
+        merge(
+          change("B", PERPETRATOR, NORMAL),
+          change("C", PERPETRATOR, VICTIM),
+          change("D", PERPETRATOR, VICTIM),
+          change("E", PERPETRATOR, VICTIM),
+          change("F", PERPETRATOR, VICTIM),
+          change("G", PERPETRATOR, VICTIM),
+          change("H", PERPETRATOR, VICTIM),
+          change("I", PERPETRATOR, VICTIM)
+        )
+      );
+      // Test 4.3.d
+      test(
+        "B",
+        CONFIRMED,
+        merge(
+          change("B", NORMAL, PERPETRATOR),
+          change("C", VICTIM, PERPETRATOR),
+          change("D", VICTIM, PERPETRATOR),
+          change("E", VICTIM, PERPETRATOR),
+          change("F", VICTIM, PERPETRATOR),
+          change("G", VICTIM, PERPETRATOR),
+          change("H", VICTIM, PERPETRATOR),
+          change("I", VICTIM, PERPETRATOR)
+        )
+      );
+
+      // Test 4.3.e
+      test(
+        "D",
+        NORMAL,
+        merge(
+          change("D", PERPETRATOR, NORMAL),
+          change("A", PERPETRATOR, VICTIM),
+          change("B", PERPETRATOR, VICTIM),
+          change("E", PERPETRATOR, VICTIM),
+          change("F", PERPETRATOR, VICTIM),
+          change("G", PERPETRATOR, VICTIM),
+          change("H", PERPETRATOR, VICTIM),
+          change("I", PERPETRATOR, VICTIM),
+          change("J", PERPETRATOR, VICTIM)
+        )
+      );
+
+      // Test 4.3.f
+      test(
+        "D",
+        CONFIRMED,
+        merge(
+          change("D", NORMAL, PERPETRATOR),
+          change("A", VICTIM, PERPETRATOR),
+          change("B", VICTIM, PERPETRATOR),
+          change("E", VICTIM, PERPETRATOR),
+          change("F", VICTIM, PERPETRATOR),
+          change("G", VICTIM, PERPETRATOR),
+          change("H", VICTIM, PERPETRATOR),
+          change("I", VICTIM, PERPETRATOR),
+          change("J", VICTIM, PERPETRATOR)
+        )
+      );
+    });
   });
 
   // ---
 
-  function initialize(graph: Dictionary<string[]>, debug?: boolean): void {
+  function initialize(graph: Dictionary<string[]>): void {
     _testCounter = 0;
     beforeAll(async () => {
       await graphService.clear();
@@ -318,9 +587,6 @@ describe("new tests", () => {
           : callees.map((callee: string) => ({ caller, callee, metrics: defaultTestMetrics }));
       });
       try {
-        // if (debug) {
-        //   console.log("ComponentCalls", JSON.stringify(componentCalls, null, 4));
-        // }
         await graphService.add([...componentCalls]);
       } catch (error) {
         throw Error(`There was an error while adding the component calls, ${error.stack}`);
@@ -338,10 +604,6 @@ describe("new tests", () => {
       let changes;
       beforeAll(async () => (changes = await graphService.updateComponentStatus(id, status)));
       it(`should return the expected changes`, () => {
-        // if (debug) {
-        //   console.log('received:', JSON.stringify(changes, null, 4));
-        //   console.log('expected:', JSON.stringify(expectedChanges, null, 4));
-        // }
         expect(changes).toEqual(expectedChanges);
       });
     });
