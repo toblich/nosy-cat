bca59b61cbca06102e76a80e4ee07b3642445870
jmoguilevsky <jmoguilevsky@gmail.com>
2021-02-27 17:32:51 -0300

    add jmeter

--- demo-services/routers/iam.ts
+++ demo-services/routers/iam.ts
@@ -3,8 +3,8 @@ import * as CLSContext from "zipkin-context-cls";
 import { Tracer, ExplicitContext, ConsoleRecorder } from "zipkin";
 import { expressMiddleware as zipkinMiddleware } from "zipkin-instrumentation-express";
 
-import * as zipkinPg from "zipkin-instrumentation-postgres";
-import * as pg from "pg";
+// import * as zipkinPg from "zipkin-instrumentation-postgres";
+// import * as pg from "pg";
 import recorder from "../recorder";
 
 const router = Router();
@@ -13,12 +13,12 @@ const ctxImpl = new CLSContext("zipkin");
 const localServiceName = process.env.NAME || "UNKNOWN"; // name of this application
 const tracer = new Tracer({ ctxImpl, recorder, localServiceName });
 
-const ZipkinPostgres = zipkinPg(tracer, pg);
-const pool = new ZipkinPostgres.Pool({
-  user: "postgres",
-  host: "postgres",
-  database: "postgres"
-});
+// const ZipkinPostgres = zipkinPg(tracer, pg);
+// const pool = new ZipkinPostgres.Pool({
+//   user: "postgres",
+//   host: "postgres",
+//   database: "postgres"
+// });
 
 // Add the Zipkin middleware
 router.use(zipkinMiddleware({ tracer, port: Number(process.env.PORT) }));
@@ -27,16 +27,16 @@ router.use(zipkinMiddleware({ tracer, port: Number(process.env.PORT) }));
 router.get("/ping", (_, res) => res.send("pong!\n"));
 
 router.get("/login", async (_, res) => {
-  const timestamp = await pool.query("SELECT NOW()");
+  // const timestamp = await pool.query("SELECT NOW()");
 
   res.status(200).json({
-    accessToken: "uuid"
+    accessToken: "uuid",
   });
 });
 
 router.get("/authorize", async (_, res) => {
   res.status(503).json({
-    message: "service down"
+    message: "service down",
   });
 });
 
