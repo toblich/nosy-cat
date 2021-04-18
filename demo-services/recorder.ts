c6cfd70e173ec30bfeda8672c30b2783739a9cd2
Tobías Lichtig <toblich@users.noreply.github.com>
2021-04-18 16:57:16 -0300

    Fix 500 on graph removal. Remove unused docker-compose services. Make zipkin URL configurable in Demo Services. (#88)
    * Fix 500 when deleting graph
    
    * Remove unused docker-compose services
    
    * Revert back to configurable zipkin uri in demo services

--- demo-services/recorder.ts
+++ demo-services/recorder.ts
@@ -8,8 +8,7 @@ const INGRESS_PORT = process.env.INGRESS_PORT;
 if (!INGRESS_HOST || !INGRESS_PORT) {
   throw Error("Missing ingress host values");
 }
-// const zipkinBaseUrl = `http://${INGRESS_HOST}:${INGRESS_PORT}`;
-const zipkinBaseUrl = `http://nosy-cat.tk:${INGRESS_PORT}`;
+const zipkinBaseUrl = `http://${INGRESS_HOST}:${INGRESS_PORT}`;
 
 // tslint:disable-next-line:no-var-requires
 const EventEmitter = require("events").EventEmitter;
